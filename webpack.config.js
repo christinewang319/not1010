f2bb1b73868c4ca579a874edba198fdae2db0cc2
Christine Wang <christinewang319@gmail.com>
2017-05-04 10:30:51 -0400

    Change t and f to true and false, and add some block functions in display.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
 var path = require('path');
 
 module.exports = {
-  context: __dirname,
   entry: './lib/not1010.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'bundle.js'
   },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '*']
-  },
+  devtool: 'source-map'
 };
