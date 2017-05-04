72751d7caed1e213c7a3e233740106809c642a4c
Christine Wang <christinewang319@gmail.com>
2017-05-03 23:14:54 -0400

    Add Board and Display classes, and create grid

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+var path = require('path');
+
 module.exports = {
+  context: __dirname,
   entry: './lib/not1010.js',
   output: {
-    path: __dirname,
-    filename: './lib/bundle.js'
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'bundle.js'
   },
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  },
 };
